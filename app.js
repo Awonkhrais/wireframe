90261a75db0d2fd7fea02bb9071e102d34cf6f94
Awonkhrais <awonkhrais@gmail.com>
2021-04-01 13:28:17 +0300

    update the wireframe with js file and link it wi html file

---
+++ app.js
@@ -0,0 +1,28 @@
+alert('Hello Iam Awon Khrais');
+/*
+   I live in Irbid,Idoun .
+   I am graduated before 2 years from Yarmouk universit from civil engineer department.
+   And now I am learning to as softwer developer.
+*/
+alert('I was born in:'+1996);
+
+var X = 25;
+alert(X+25);
+
+var MyGithupUsername='Githup account:awonkhrais';
+document.write(MyGithupUsername);
+
+var x=  prompt('enter you age','25');
+
+alert(x/2);
+
+if(x<0) {
+alert('you input a negative value')
+}
+else{
+    alert('You input a positive value');
+}
+if(x==25)
+{
+alert(25);
+}
